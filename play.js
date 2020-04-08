a9b2158142fe0057d6b7c458e4ab9c1036a67d40
Jessie Wright <jessie.wright@gmail.com>
2020-04-08 16:25:54 +0000

    refactored to seperate input functions into own module, updated import/export syntax for client

--- play.js
+++ play.js
@@ -1,22 +1,6 @@
 const connect = require('./client');
+const {setupInput} = require('./input');
 
-const handleUserInput = (key) => {
-  if(key === '\u0003') {
-    process.exit();
-  }
-}
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-}
 
 console.log('Connecting ...');
 connect();
